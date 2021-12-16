5f788ed96c9949b2a57ef4ee0f35501a557a409d
yechubot <yechukim217@gmail.com>
2021-12-16 13:44:25 +0900

    fix: full screen width

--- App.js
+++ App.js
@@ -1,27 +1,15 @@
-
 import React from 'react'
-import { FlatList, StyleSheet, View, Text, SafeAreaView, Animated } from 'react-native';
+import { FlatList, StyleSheet, Text, Animated, Image } from 'react-native';
+import { useAnimatedGestureHandler } from 'react-native-reanimated';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import useWindowDimensions from 'react-native/Libraries/Utilities/useWindowDimensions';
+import 'react-native-get-random-values'
+import { v4 as uuid4 } from 'uuid';
 
-const CARD_HEIGHT = 300
-
-const DATA = [
-  {
-    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-    title: 'First Item',
-    color: 'pink'
-  },
-  {
-    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-    title: 'Second Item',
-    color: 'yellow'
-  },
-  {
-    id: '58694a0f-3da1-471f-bd96-145571e29d72',
-    title: 'Third Item',
-    color: 'white'
-  },
-];
+const data = Array.from({ length: 10 })
+  .map((_, index) => ({
+    id: uuid4()
+  }))
 
 const AnimatedFlatList = Animated.createAnimatedComponent(FlatList)
 
@@ -33,26 +21,28 @@ function App() {
   })
 
   return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: '#ddd' }}>
-      <AnimatedFlatList
-        decelerationRate={'fast'}
-        snapToInterval={height}
-        // snapToOffsets
-        snapToAlignment={'start'}
-        scrollsToTop={true}
-        contentContainerStyle={{ paddingHorizontal: 16 }}
-        scrollEventThrottle={16}
-        bounces={false}
-        data={DATA}
-        renderItem={({ index, item }) => (
-          <Card {...{ index, y, item }} />
-        )
-        }
-        keyExtractor={item => item.id}
-        {...{ onScroll }}
-      />
+    <SafeAreaProvider>
+      <SafeAreaView
+        style={{ flex: 1, backgroundColor: '#fff' }}>
+        <AnimatedFlatList
+          decelerationRate={'fast'}
+          snapToInterval={height} // stops when figner lifts ? i guess 
+          // snapToOffsets
+          snapToAlignment={'start'}
+          scrollsToTop={true}
+          scrollEventThrottle={16}
+          bounces={false}
+          data={data}
+          renderItem={({ index, item }) => (
+            <Card {...{ index, y, item }} />
+          )
+          }
+          keyExtractor={item => item.id}
+          {...{ onScroll }}
+        />
 
-    </SafeAreaView>
+      </SafeAreaView>
+    </SafeAreaProvider >
 
   );
 }
@@ -87,24 +77,28 @@ const Card = ({ index, y, item }) => {
   })
   return (
     <Animated.View
-      style={{
-        backgroundColor: item.color,
-        width: 350,
-
-        height: height,
+      style={[styles.block, {
         transform: [{ translateY }, { scale }],
         opacity,
-      }} >
-      <Text style={styles.text}>{item.title}</Text>
+        height: height,
+        width: width
+      }]}>
+      <Image
+        resizeMode='cover'
+        style={{ flex: 1 }}
+        source={{ uri: `https://picsum.photos/${width}/${height}` }}
+      />
     </Animated.View>
   )
 }
 const styles = StyleSheet.create({
-
+  block: {
+    flex: 1,
+    padding: 10
+  },
   text: {
     fontSize: 24,
   }
-
 })
 
 
